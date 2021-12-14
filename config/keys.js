f33a788d3dea0b33e8c9c774fdd405708b77738b
shahriar31028 <50696940+shahriar31028@users.noreply.github.com>
2021-12-14 13:39:05 +0600

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://shahriar:1234@databaseproject.8j8j7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+  userRoles: ["user", "manager"],
+};
